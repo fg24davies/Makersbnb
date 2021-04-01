3ec65cef4f26683ba0bff87425ed9f09362ceb32
Yates101 <alecyates94@gmail.com>
2021-04-01 15:26:58 +0100

    final css commit

--- spec/features/request_to_book_spec.rb
+++ spec/features/request_to_book_spec.rb
@@ -1,8 +1,7 @@
 feature "Requesting to book a property" do
   scenario "Logged-in user can request to book for a particular date" do
     log_in(username: 'taran_1', password: 'password123')
-    click_button('View Listings')
-    click_button('Spare Room')
+    first('.listing').click_button('View')
     fill_in('date', with: '2021-04-15')
     click_button('Request to book')
     expect(page).to have_content('Thank you! Your booking request for Spare Room on 2021-04-15 has been sent.')
